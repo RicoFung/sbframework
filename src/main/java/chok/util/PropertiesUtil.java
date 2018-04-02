a174a7f2036a01dba74a92bf10e40e0582f2c1ef
RicoFung <ricofungcn@icloud.com>
2018-04-02 15:09:13 +0800

    fix PropertiesUtil

--- src/main/java/chok/util/PropertiesUtil.java
+++ src/main/java/chok/util/PropertiesUtil.java
@@ -9,11 +9,16 @@ public class PropertiesUtil
 
 	public static String getValue(String key)
 	{
+		return getValue("", key);
+	}
+	
+	public static String getValue(String dir, String key)
+	{
 		if (prop == null)
 			prop = new Properties();
 		try
 		{
-			prop.load(PropertiesUtil.class.getClassLoader().getResourceAsStream("config/application.properties"));
+			prop.load(PropertiesUtil.class.getClassLoader().getResourceAsStream(dir + "application.properties"));
 			return prop.getProperty(key);
 		}
 		catch (IOException e)
